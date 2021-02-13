162c62fe4434b3cac4d71d6979a35ce6466d17bf
erwin alam <djancok901@gmail.com>
2021-02-14 00:55:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
+import './index.css';
 import './App.css';
+
+function Navbar() {
+    return (
+      <div className="py-5">
+        <a href="#" className="text-indigo-500 font-bold px-4 py-3">Home</a>
+        <a href="#" className="text-indigo-500 font-bold px-4 py-3">Projects</a>
+        <a href="#" className="text-indigo-500 font-bold px-4 py-3">Contact</a>
+        <button className="bg-indigo-500 hover:bg-indigo-700 text-white px-3 py-2 rounded-md">Hire Me</button>
+      </div>
+    );
+}
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, My name is Erwin Alam
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
